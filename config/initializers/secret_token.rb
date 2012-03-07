f17ccd817653e7e511a02b76a16018673bc62a41
ohergal <ohergal@gmail.com>
2012-03-07 12:30:02 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xmadweb::Application.config.secret_token = 'ccc91e58d2e56fda5822f9b8a80c1a9bb34f82943a07ea1a0b7680328a0293faceff9d56931c3dd2c462f6f1f2c6e5c22b0ee0e67841a5bab0445dce28114382'
