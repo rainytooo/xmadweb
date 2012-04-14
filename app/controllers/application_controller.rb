8bd859ca247b5493c0b4db4985b83c56ae8ae77b
ohergal <ohergal@gmail.com>
2012-04-14 15:40:41 +0800

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     end 
     # 没有身份
     def authorize_no_role!
-      unless current_user.role == 0
+      unless current_user.role != 0
         flash[:alert] = "您的登录身份太低,不能完成次操作"
         redirect_to root_path
       end
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
     def authorize_not_student!
       authenticate_user!
       authorize_no_role!
-      unless current_user.role == 1
+      unless current_user.role != 1
         flash[:alert] = "您的登录身份必须为助理以上级别才能完成次操作"
         redirect_to root_path
       end
