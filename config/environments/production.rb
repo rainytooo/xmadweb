fdd53d1e7b4226d2810b22c2d0363dfa74964a34
ryan007 <goodboyryan007@gmail.com>
2012-12-29 10:31:55 +0800

    更改邮件配置

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Xmadweb::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
@@ -64,19 +64,19 @@ Xmadweb::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.host_url = "http://dept.vip.xiaoma.com/"
-  config.staticfile_server_url = "http://dept.vip.xiaoma.com/"
+  # config.host_url = "http://dept.vip.xiaoma.com/"
+  # config.staticfile_server_url = "http://dept.vip.xiaoma.com/"
    
    
-  config.action_mailer.default_url_options = { :host => 'dept.vip.xiaoma.com' }
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { :host => 'dept.vip.xiaoma.com' }
+  # config.action_mailer.delivery_method = :smtp
    
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.qq.com",
-    :port                 => 25,
-    :domain               => 'www.xiaoma.com',
-    :user_name            => 'noreply@xiaoma.com',
-    :password             => '1q2w3e4r',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.qq.com",
+  #   :port                 => 25,
+  #   :domain               => 'www.xiaoma.com',
+  #   :user_name            => 'noreply@xiaoma.com',
+  #   :password             => '1q2w3e4r',
+  #   :authentication       => 'plain',
+  #   :enable_starttls_auto => true  }
 end
