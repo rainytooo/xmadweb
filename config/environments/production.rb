d53ab35569741d02cd00fe7d1893043fec027358
ryan007 <goodboyryan007@gmail.com>
2012-12-29 10:29:11 +0800

    更改邮件配置

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Xmadweb::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
@@ -72,7 +72,7 @@ Xmadweb::Application.configure do
   config.action_mailer.delivery_method = :smtp
    
   config.action_mailer.smtp_settings = {
-    :address              => "localhost",
+    :address              => "smtp.qq.com",
     :port                 => 25,
     :domain               => 'www.xiaoma.com',
     :user_name            => 'noreply@xiaoma.com',
